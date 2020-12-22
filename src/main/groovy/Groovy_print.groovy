0820fa09e0bccb0c84d7e7dd83456908d1e7a471
LEE_CHANG_HUMUS_DESKTOP <dlckd125@humuson.com>
2020-12-22 09:44:19 +0900

    groovy print test

---
+++ src/main/groovy/Groovy_print.groovy
@@ -0,0 +1,14 @@
+class Groovy_print {
+    static void main(String[] args) {
+        def fst = "Hello World"
+        def sec = "Korea"
+
+        def a = 10
+        def b = 5
+        println a+10
+
+        println "${fst}, ${sec}"
+        println "${fst + sec}"
+        println "a + b = ${a + b}"
+    }
+}
